b35e95032f90f7a2ab4bd976b30b7afbca2a8808
Elizabeth Hagearty <elizabeth@glassymedia.com>
2014-07-23 16:27:46 -0400

    added suggestion for duration under 10 days to account for extrapolation error

--- js/kickstarter_analysis.js
+++ js/kickstarter_analysis.js
@@ -53,9 +53,14 @@ function ChangePieChart() {
     object.data = "Charts/" + category + ".svg";
 }
 
-function suggest(category, duration, goal, avg_goal, avg_length) {
+function suggest(category, duration, goal, avg_goal, avg_length, short_goal) {
+    if (duration < 10) {
+        if (goal > short_goal) {
+            return "WARNING: this estimate is extrapolated, for under 10 days the highest goal of a success project was " + short_goal + ". We suggest picking a more realistic duration/goal combination!";
+        }
+    }
     if (goal > avg_goal) {
-        return "The average goal for " + category + " is " + avg_goal + "! keep that in mind."
+        return "The average goal for this category is " + avg_goal + "! keep that in mind.";
     }
     return "Good luck!"
 }
@@ -65,22 +70,46 @@ function form_suggestions(category, duration, goal) {
     var s = document.getElementById("suggestions")
     switch (category) {
         case 'Art':
-            s.textContent = suggest(category, duration, goal, 10000, 30);
+            s.textContent = suggest(category, duration, goal, 10000, 30, 12000);
             break;
         case 'Comics':
-            s.textContent = suggest(category, duration, goal, 1000, 30);;
+            s.textContent = suggest(category, duration, goal, 1000, 30, 2000);
             break;
         case 'Dance':
-            s.textContent = suggest(category, duration, goal, 1000, 30);;
+            s.textContent = suggest(category, duration, goal, 1000, 30, 8000);
             break;
         case 'Design':
-            s.textContent = suggest(category, duration, goal, 10, 30);
+            s.textContent = suggest(category, duration, goal, 10, 30, 5000);
             break;
         case 'Fashion':
-            s.textContent = suggest(category, duration, goal, 10000, 30);;
+            s.textContent = suggest(category, duration, goal, 10000, 30, 10000);
             break;
         case 'Film and Video':
-            s.textContent = suggest(category, duration, goal, 100, 30);;
+            s.textContent = suggest(category, duration, goal, 100, 30, 50000);
+            break;
+        case 'Food':
+            s.textContent = suggest(category, duration, goal, 10000, 30, 10000);
+            break;
+        case 'Games':
+            s.textContent = suggest(category, duration, goal, 1000, 30, 125000);
+            break;
+        case 'Journalism':
+            s.textContent = suggest(category, duration, goal, 1000, 30, 3500);
+            break;
+        case 'Music':
+            s.textContent = suggest(category, duration, goal, 10, 30, 37500);
+            break;
+        case 'Photography':
+            s.textContent = suggest(category, duration, goal, 10, 30, 7500);
+            break;
+        case 'Publishing':
+            s.textContent = suggest(category, duration, goal, 10000, 30, 13500);
+            break;
+        case 'Technology':
+            s.textContent = suggest(category, duration, goal, 100, 30, 5000);
+            break;
+        case 'Theater':
+            s.textContent = suggest(category, duration, goal, 100, 30, 3500);
             break;
     }
 }
